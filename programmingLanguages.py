4e125a9ba10d40dfc88ae618b82c8416e0975e03
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-08-13 22:51:45 +0000

    Add code to get the programming languages from wikipedia

--- programmingLanguages.py
+++ programmingLanguages.py
@@ -1,23 +1,32 @@
+from bs4 import BeautifulSoup
 from bs4.element import ResultSet, Tag
 from requests import get
-from bs4 import BeautifulSoup
 
-def getWikiPage()    ->  BeautifulSoup:
+
+def getWikiPage() -> BeautifulSoup:
     page = get(url="https://en.wikipedia.org/wiki/List_of_programming_languages").text
 
     return BeautifulSoup(markup=page, features="lxml")
 
-def getProgrammingLanguage(soup: BeautifulSoup) ->  set:
+
+def getProgrammingLanguage(soup: BeautifulSoup) -> set:
     data: set = set()
 
-    letters: ResultSet = getWikiPage().findChildren(name="div", attrs={"class":"div-col"})
+    letters: ResultSet = soup.findChildren(name="div", attrs={"class": "div-col"})
+
+    letter: Tag
+    for letter in letters:
+
+        languages: ResultSet = letter.findAll("li")
+
+        language: Tag
+        for language in languages:
+            data.add(language.text)
+
+    return sorted(data)
 
 
+q: BeautifulSoup = getWikiPage()
 
-q: BeautifulSoup = getProgrammingLanguage()
 
-x: Tag
-for x in t:
-    data: ResultSet = x.findChildren(name="li")
-
-    print(data)
+print(getProgrammingLanguage(soup=q))

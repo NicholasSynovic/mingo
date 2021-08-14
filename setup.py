ee773b10dcf1ac53f4c01a4ef4ac31f5649a0e10
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-08-14 11:31:09 -0500

    Change name of build

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mingo-nsynovic",
+    name="mingo",
     version="0.0.4",
     author="Nicholas M. Synovic",
     author_email="nicholas.synovic@gmail.com",

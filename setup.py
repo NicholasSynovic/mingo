19aa4b7d63c1c189ddcf50fb90f29869134bb260
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-08-14 15:23:18 +0000

    Add build files

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mingo-nsynovic",
+    version="0.0.1",
+    author="Nicholas M. Synovic",
+    author_email="nicholas.synovic@gmail.com",
+    description="A Python project to get a list of programming languages from Wikipedia",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NicholasSynovic/mingo",
+    project_urls={
+        "Bug Tracker": "https://github.com/NicholasSynovic/mingo/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+    install_requires=["bs4", "lxml", "requests"],
+)
